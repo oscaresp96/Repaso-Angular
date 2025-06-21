5df8d7f4b6b16520219bdfe740ab00215a86d53e
oscaresp96 <orlandoseas18@gmail.com>
2025-06-20 19:57:24 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ListarDoctoresComponent } from './componentes/listar-doctores/listar-doctores.component';
-import { AgregarDoctoresComponent } from './componentes/agregar-doctores/agregar-doctores.component';
+import { MenuComponent } from './componentes/menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ListarDoctoresComponent, AgregarDoctoresComponent],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListarDoctoresComponent } from './componentes/listar-doctores/listar-doctores.component';
+import { AgregarDoctoresComponent } from './componentes/agregar-doctores/agregar-doctores.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'listar', component: ListarDoctoresComponent},
+    {path: 'agregar', component: AgregarDoctoresComponent},
+    {path: '', redirectTo: 'listar', pathMatch: 'full'},
+    {path: '**', redirectTo: 'listar'}
+];
